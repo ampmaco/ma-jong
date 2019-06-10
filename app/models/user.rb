97538b82d291d21fc1ae1c2d3cad2d4e742ba817
林　晃弘 <ampmaco@gmail.com>
2019-06-10 18:33:31 +0900

    Postモデル、コントローラーの作成、ルート設定、Usersルートとビューファイル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+# deviseの初期設定
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+# ユーザー情報のバリデーション
+  validates :user_name, presence: true, length: {maximum: 20}
+  validates :email, presence: true, uniqueness: true, length: {maximum: 254}
+  validates :profile, length: {maximum: 500}
+
 end
