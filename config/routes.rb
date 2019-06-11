90700d6a7945ca7c799573cc73b366fb76ed3428
林　晃弘 <ampmaco@gmail.com>
2019-06-11 17:06:46 +0900

    トップページのメインビジュアル実装、ルートパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root :to => "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update]
   resources :posts, only: [:index, :new, :create, :edit, :patch, :destroy]
