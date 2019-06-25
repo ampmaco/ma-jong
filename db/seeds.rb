8edd945e4da3c5bca4dd6554b4fef345ac2fdd74
林　晃弘 <ampmaco@gmail.com>
2019-06-25 17:26:05 +0900

    デプロイ用画像ファイルパス変更、SEEDファイルにて管理者を設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  User.create!(
+    email:     'ampmaco@gmail.com',
+    password:  'ampmaco',
+    user_name: '管理者',
+    addres:    '東京都渋谷区神南１丁目１９−１１',
+    admin:     'true',
+    )
